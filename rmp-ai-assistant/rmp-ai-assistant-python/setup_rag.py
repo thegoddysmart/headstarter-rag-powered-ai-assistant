718aff7da9e7348b06529af633ff63400bc1e7b0
Godfred <goddysmart2468@gmail.com>
2024-08-22 02:41:53 +0000

    Foundation for your RAG system

--- rmp-ai-assistant/rmp-ai-assistant-python/setup_rag.py
+++ rmp-ai-assistant/rmp-ai-assistant-python/setup_rag.py
@@ -1,33 +1,55 @@
 from dotenv import load_dotenv
 load_dotenv()
 from pinecone import Pinecone, ServerlessSpec
-import requests
+import google.generativeai as genai
 import os
 import json
 
+# Load environment variables from .env.local
+load_dotenv(dotenv_path='../.env.local')
+
 # Initialize Pinecone
-pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
+pc = Pinecone(api_key=os.environ["PINECONE_API_KEY"])
+
+# Check if the index already exists
+index_name = "rag"
+existing_indexes = pc.list_indexes()
+
+if index_name in existing_indexes:
+    pc.delete_index(index_name)  # Delete existing index
 
 # Create a Pinecone index
 pc.create_index(
-    name="rag",
-    dimension=1536,
+    name=index_name,
+    dimension=768,
     metric="cosine",
     spec=ServerlessSpec(cloud="aws", region="us-east-1"),
 )
+
+# Configure Gemini API
+genai.configure(api_key=os.environ["GOOGLE_GENERATIVE_AI_API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 # Load the review data
 data = json.load(open("reviews.json"))
 
 processed_data = []
-client = OpenAI()
+
+# Function to create embeddings using Gemini
+def create_embedding_gemini(text):
+    result = genai.embed_content(
+        model="models/text-embedding-004",
+        content=text,
+        task_type="retrieval_document",
+        title="Embedding of single string"
+    )
+    print(result)
+    
+    return result['embedding']
 
 # Create embeddings for each review
 for review in data["reviews"]:
-    response = client.embeddings.create(
-        input=review['review'], model="text-embedding-3-small"
-    )
-    embedding = response.data[0].embedding
+    embedding = create_embedding_gemini(review['review'])
     processed_data.append(
         {
             "values": embedding,
